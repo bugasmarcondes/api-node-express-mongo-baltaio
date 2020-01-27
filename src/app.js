1c3889e64ba6c0848b1308f6566b0a8ebb9e0e03
Bugas <bugasmarcondes@MacBook-Pro-de-Bugas.local>
2020-01-27 06:21:11 -0300

    Separando as rotas em arquivos isolados

--- src/app.js
+++ src/app.js
@@ -6,35 +6,14 @@ const bodyParser = require('body-parser');
 const app = express();
 const router = express.Router();
 
+// carrega as rotas
+const indexRoute = require('./routes/index-route');
+const productRoute = require('./routes/product-route');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const routeGet = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: 'API NodeJS',
-        version: '0.0.1'
-    });
-});
-
-const routePost = router.post('/', (req, res, next) => {
-    res.status(201).send(req.body);
-});
-
-const routePut = router.put('/:id', (req, res, next) => {
-    const id = req.params.id;
-    res.status(201).send({
-        id: id,
-        item: req.body
-    });
-});
-
-const routeDelete = router.delete('/', (req, res, next) => {
-    res.status(200).send(req.body);
-});
-
-app.use('/', routeGet);
-app.use('/', routePost);
-app.use('/', routePut);
-app.use('/', routeDelete);
+app.use('/', indexRoute);
+app.use('/products', productRoute);
 
 module.exports = app; 
