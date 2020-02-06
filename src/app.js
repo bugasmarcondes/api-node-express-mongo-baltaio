3fd19ebbcd35b6c5940a90155477529d90d683c9
Bugas <bugasmarcondes@MacBook-Pro-de-Bugas.local>
2020-02-06 07:44:49 -0300

    Habilitando formatOnSave utilizando Prettier

--- src/app.js
+++ src/app.js
@@ -22,18 +22,28 @@ const productRoute = require('./routes/product-route');
 const customerRoute = require('./routes/customer-route');
 const orderRoute = require('./routes/order-route');
 
-app.use(bodyParser.json({
-    limit: '5mb'
-}));
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(
+    bodyParser.json({
+        limit: '5mb',
+    })
+);
+app.use(
+    bodyParser.urlencoded({
+        extended: false,
+    })
+);
 
 // Habilita o CORS (cross origin resource sharing)
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, x-access-token'
+    );
+    res.header(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, DELETE, OPTIONS'
+    );
     next();
 });
 
